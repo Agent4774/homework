839594c04ab78bc54efbbc8b8bff5939cde20773
Sasha <sasha@sasha.com>
2020-04-28 21:51:33 +0300

    Homework_2 commit

---
+++ homework_2.js
@@ -0,0 +1,20 @@
+'use strict';
+
+for(let i = 1; i < 10; i++){
+    console.log(i);
+}
+
+// let i = 1;
+// do{
+//     console.log(i);
+//     i++;
+// } while (i < 10);
+
+// let i = 1
+// while(i < 10){
+//     console.log(i);
+//     i++;
+// }
+
+//Существуют 3 вида циклов в JS
+
